4e4f4c7497210dffc2ef2e50f1c93eb6e3ad4548
Данил Колесник <kolesnik@rollbyte.com>
2020-07-22 20:31:13 +1000

    IONDVSERV-1

--- errors/backend-errors.js
+++ errors/backend-errors.js
@@ -3,6 +3,7 @@ const prefix = 'rest.backend';
 module.exports = {
   NOT_FOUND_CONV: `${prefix}.notfoundconv`,
   WRONG_OPTIONS: `${prefix}.wrongoptions`,
+  HTTP_401: `${prefix}.401`,
   HTTP_403: `${prefix}.403`,
   HTTP_404: `${prefix}.404`,
   HTTP_500: `${prefix}.500`
